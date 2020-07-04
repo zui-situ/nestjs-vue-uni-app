43532d0a761891e825c8a062962771111c27d334
zui-situ <382808911@qq.com>
2020-07-04 19:17:55 +0800

    first commit

---
+++ web/pages/courses/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="pa-3">
+        <h3>热门课程</h3>
+        <v-row>
+            <v-col v-for="item in courses.data" :key="item._id">
+                <v-card class="mx-auto" max-width="344" :to="`/courses/${item._id}`">
+                    <v-img :src="item.cover" height="200px"></v-img>
+                    <v-card-title>{{item.name}}</v-card-title>
+                    <v-card-subtitle>{{item.createdAt}}</v-card-subtitle>
+                    <v-card-actions>
+                        <v-btn text>Share</v-btn>
+                        <v-btn color="purple" text>Explore</v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    async asyncData({ $axios }){
+        const courses = await $axios.$get('courses')
+        console.log(courses);
+        return {
+            courses
+        }
+    }
+}
+</script>
+<style></style>
